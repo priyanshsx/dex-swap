7e4ef32044ad25b938ca4605742c98617d47bf02
Priyansh <saxenapriyansh95@gmail.com>
2026-09-16 07:21:02 +0530

    added the initial structure

--- app/app.py
+++ app/app.py
@@ -2,3 +2,8 @@ import pandas as pd
 import numpy as np 
 import plotly.graph_objects as go 
 from plotly.subplots import make_subplots
+
+# query data from dune and export as csv 
+# timeline: past 90 days 
+# to pull: block_time, project, amount_usd, token_bought_amount, token_sold_amount, fee tier (uniswap v3)
+# from projects: uniswap, sushiswap, curve 
